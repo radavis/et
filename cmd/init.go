4bb4e18146b1677058c477cef8bd6836d8d7b267
Spencer Dixon <spencercdixon@gmail.com>
2017-04-22 12:03:40 -0400

    Adds real init func which will write to .et.yaml file in home directory

--- cmd/init.go
+++ cmd/init.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
 
+	"github.com/mitchellh/go-homedir"
 	. "github.com/radavis/et/common"
 	"github.com/spf13/cobra"
 )
@@ -15,9 +19,21 @@ var initCmd = &cobra.Command{
 		username := StringRequired("Enter your username: ")
 		token := StringRequired("Enter your ET token: ")
 		host := StringRequired("Host for ET: ")
-		fmt.Println(username)
-		fmt.Println(token)
-		fmt.Println(host)
+
+		configTemplate := fmt.Sprintf(`username: %s
+token: %s
+host: %s
+`, username, token, host)
+		home, err := homedir.Dir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		configPath := path.Join(home, ".et.yaml")
+
+		err = ioutil.WriteFile(configPath, []byte(configTemplate), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
