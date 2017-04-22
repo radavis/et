b75cb228fb12846c4ea4fc7e48e8457e8213355c
Richard Davis <rdavis.bacs@gmail.com>
2017-04-22 14:28:23 -0400

    download archive

--- cmd/get.go
+++ cmd/get.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/radavis/et/horizon"
 	"github.com/spf13/cobra"
@@ -12,8 +12,14 @@ var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		lesson := horizon.GetLesson("golf-score")
-		fmt.Printf("%v\n", lesson.ArchiveUrl)
+		if len(args) <= 0 {
+			log.Fatal("missing slug name")
+		}
+
+		slug := args[0]
+		lesson := horizon.GetLesson(slug)
+		// fmt.Printf("%v\n", lesson.ArchiveUrl)
+		lesson.GetArchive()
 	},
 }
 
--- horizon/lesson.go
+++ horizon/lesson.go
@@ -1,5 +1,12 @@
 package horizon
 
+import (
+	"fmt"
+	"os"
+	"net/http"
+	"io"
+)
+
 type Lesson struct {
 	Slug       string `json:"slug,omitempty"`
 	Title      string `json:"title,omitempty"`
@@ -10,4 +17,29 @@ type Lesson struct {
 
 type Lessons struct {
 	Lessons []Lesson `json:"lessons"`
+}
+
+func (l *Lesson) GetArchive() {
+	filename := fmt.Sprintf("%s.tar.gz", l.Slug)
+	output_file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error while creating", filename, "-", err)
+		return
+	}
+	defer output_file.Close()
+
+	response, err := http.Get(l.ArchiveUrl)
+	if err != nil {
+		fmt.Println("Error while downloading", l.ArchiveUrl, "-", err)
+		return
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output_file, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", l.ArchiveUrl, "-", err)
+		return
+	}
+
+	fmt.Println(n, "bytes downloaded.")
 }
